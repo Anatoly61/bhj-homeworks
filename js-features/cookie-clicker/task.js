d087a12b5c02d7e2ef6cd35d9ebdbc8b4d4d7c2e
Anatoly61 <128617494+Anatoly61@users.noreply.github.com>
2023-07-15 19:48:10 +0300

    Изменения от 15.07.2023

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,11 @@
+const cookieImage = document.getElementById("cookie");
+let cookieClick = document.getElementById("clicker__counter");
+let previousClicks = 0;
+cookieImage.onclick = function () {
+    let currentClicks = parseInt(cookieClick.textContent);
+    cookieClick.textContent = currentClicks + 1;
+     if (currentClicks % 2 === 0){
+        cookieImage.width = 250
+     }
+     else cookieImage.width = 200
+      }
