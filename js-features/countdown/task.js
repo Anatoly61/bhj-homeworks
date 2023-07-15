31fe8477e176c2f0fdf4cbec725d7991c80b8299
Anatoly61 <128617494+Anatoly61@users.noreply.github.com>
2023-07-15 19:48:58 +0300

    Изменения от 15.07.2023

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+const countTime  = function(){
+    const output = document.getElementById("timer");
+
+    if (output.textContent >= 1){
+        output.textContent --;
+    }	else if(output.textContent === '0') {
+        alert("Вы победили в конкурсе!");    
+        document.location.assign('https://developer.mozilla.org/ru/docs/Web/API/Location.reload');   
+    }
+}
+
+setInterval(countTime,1000);
